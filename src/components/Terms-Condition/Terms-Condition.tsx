c0c106ab196ada5e3ffb7218f5f1ea1fd9516ced
KarishmaVJCS <131963107+KarishmaVJCS@users.noreply.github.com>
2025-09-03 15:33:54 +0530

    terms and condition page changes

--- src/components/Terms-Condition/Terms-Condition.tsx
+++ src/components/Terms-Condition/Terms-Condition.tsx
@@ -2,199 +2,112 @@
 
 import PageBaner from "../PageBaner/PageBaner";
 import "./Terms-Condition.css";
-import Image from "next/image";
-import aboutImage from "@image/about/about-square-8.webp";
 
 const TermsCondition = () => {
   return (
     <section className="terms-section">
-      <PageBaner heading={"Terms Of Use"} description={" "}></PageBaner>
-      <div className="container mt-3">
-        <div className="row">
-          {/* Text Content */}
+      <PageBaner heading={"Terms Of Use"} description={" "} />
+
+      <div className="container mt-5">
+        <div className="row g-4">
+          {/* Left Column */}
           <div className="col-lg-6 terms-text">
             <h2 className="site-url">Terms of Use for www.vjcs.com</h2>
 
             <p>
-              This Website and the information, tools, and material contained in
-              it are not directed to, or intended for distribution to or use by,
-              any person or entity who is a citizen or resident of or located in
-              any jurisdiction where such distribution, publication,
-              availability or use would be contrary to law or regulation or
-              which would subject VJ Coresoft Pvt. Ltd. (“VJCS”) to any
-              registration or licensing requirement within such jurisdiction.
+              This Website and the information, tools, and material contained in it
+              are not directed to, or intended for distribution to or use by, any
+              person or entity who is a citizen or resident of or located in any
+              jurisdiction where such distribution, publication, availability or use
+              would be contrary to law or regulation or which would subject VJ
+              Coresoft Pvt. Ltd. (“VJCS”) to any registration or licensing requirement
+              within such jurisdiction.
             </p>
 
             <p className="highlight">
-              This web site is subject to periodic update and revision.
-              Materials should only be considered current as of the date of
-              initial publication appearing thereon, without regard to the date
-              on which you may access the information. VJCS maintains the right
-              to delete or modify information on this web site without prior
-              notice.
+              This web site is subject to periodic update and revision. Materials
+              should only be considered current as of the date of initial publication
+              appearing thereon. VJCS maintains the right to delete or modify
+              information on this web site without prior notice.
             </p>
+
             <h5>Limited License</h5>
             <p>
-              Subject to the terms and conditions set forth in these “Terms of
-              Use”, VJCS grants you a non-exclusive, non-transferable, limited
-              right to access, use and display this site and the materials
-              thereon.
+              Subject to the terms and conditions set forth in these “Terms of Use”,
+              VJCS grants you a non-exclusive, non-transferable, limited right to
+              access, use and display this site and the materials thereon.
             </p>
 
             <p>You hereby agree and confirm:</p>
             <ul>
-              <li>
-                Access to this web site and the information contained herein is
-                not unlawful under applicable laws.
-              </li>
-              <li>
-                Not to circulate copies of this information in any manner
-                (including photocopying and email).
-              </li>
-              <li>
-                Not to reproduce, retransmit, distribute, disseminate, sell,
-                publish, broadcast or circulate the contents.
-              </li>
-              <li>
-                Not to interrupt or attempt to interrupt the operation of this
-                web site in any way.
-              </li>
-              <li>
-                Not to intrude or attempt to intrude into this web site in any
-                way.
-              </li>
-              <li>
-                Not to post any obscene, defamatory or annoying materials.
-              </li>
-              <li>
-                Not to obscure any materials, including this notice, already
-                posted.
-              </li>
-              <li>
-                Not to use the web site or its contents to defame, intimidate,
-                annoy or cause nuisance.
-              </li>
+              <li>Access to this web site and the information contained herein is not unlawful under applicable laws.</li>
+              <li>Not to circulate copies of this information in any manner (including photocopying and email).</li>
+              <li>Not to reproduce, retransmit, distribute, disseminate, sell, publish, broadcast or circulate the contents.</li>
+              <li>Not to interrupt or attempt to interrupt the operation of this web site in any way.</li>
+              <li>Not to intrude or attempt to intrude into this web site in any way.</li>
+              <li>Not to post any obscene, defamatory or annoying materials.</li>
+              <li>Not to obscure any materials, including this notice, already posted.</li>
+              <li>Not to use the web site or its contents to defame, intimidate, annoy or cause nuisance.</li>
             </ul>
 
             <h5>Third Party Content</h5>
             <p>
-              The VJCS Web Site makes information of third parties available,
-              including articles, analyst reports, news reports, tools to
-              facilitate calculation, company information and data about
-              financial markets and other data You acknowledge and agree that
-              the Third Party Content is not created or endorsed by VJCS nor are
-              any products or services offered through it. The provision of
-              Third Party Content is for general informational purposes only.
-              You acknowledge that the Third Party Content provided to you is
-              obtained from sources believed to be reliable, but that no
-              guarantees are made by the Web Site or the providers of the Third
-              Party Content as to its accuracy, completeness, timeliness.
+              The VJCS Web Site makes information of third parties available, including
+              articles, analyst reports, news reports, tools to facilitate calculation,
+              company information and data about financial markets. You acknowledge
+              and agree that the Third Party Content is not created or endorsed by
+              VJCS. The provision of Third Party Content is for informational purposes
+              only and VJCS makes no guarantees about accuracy or completeness.
             </p>
-            <p>
-              By using any Third Party Content, you may leave this Web Site and
-              be directed to an external website, or to a website maintained by
-              an entity other than VJCS. If you decide to visit any such site,
-              you do so at your own risk and it is your responsibility to take
-              all protective measures to guard against viruses or other
-              destructive elements. VJCS makes no warranty or representation
-              regarding, and does not endorse, any linked Web sites or the
-              information appearing thereon or any of the products or services
-              described thereon.
-            </p>
-             <h5>No Warranties</h5>
-              <p>
-                This web site, the information and materials on this web site,
-                are provided without any representation or warranty, express or
-                implied, of any kind, including, but not limited to, warranties
-                of merchantability, non infringement, or fitness for any
-                particular purpose. There is no warranty of any kind, express or
-                implied, regarding third party content. There is also no
-                warranty that this web site will be free of any computer
-                viruses. Some jurisdictions do not allow for the exclusion of
-                implied warranties, so the above exclusions may not apply to
-                you.
-              </p>
-              <h5>International Users and Choice of Law</h5>
-              <p>
-                The foregoing are subject to the laws of the Republic of India
-                and the courts in Pune, India shall have the exclusive
-                jurisdiction on any dispute that may arise out of the use of
-                this site.
-              </p>
-              <p>Please proceed only if you accept all the conditions enumerated herein above, out of your free will and consent.</p>
           </div>
 
-          {/* Image Content */}
-          <div className="col-lg-6 terms-image terms-text">
-            <div className="">
-               <Image
-                src={aboutImage}
-                alt="About Us"
-                className="rounded-4 shadow custom-about-img"
-               
-              />
-            </div>
-            <div className=" mt-5">
-             
+          {/* Right Column */}
+          <div className="col-lg-6 terms-text">
+            <h5>No Warranties</h5>
+            <p>
+              This web site, the information and materials on this web site, are
+              provided without any representation or warranty, express or implied.
+              There is no warranty of any kind, including merchantability or fitness
+              for purpose. Some jurisdictions do not allow the exclusion of implied
+              warranties, so the above exclusions may not apply to you.
+            </p>
 
-              <h5>Limitation of Damages</h5>
-              <p>
-                In no event shall VJCS and / or its subsidiaries be liable for
-                any direct, indirect, punitive, incidental, special,
-                consequential damages or any damages whatsoever including,
-                without limitation, damages for loss of use, data or profits,
-                arising out of or in any way connected with the use or
-                performance of this web site / services, with the delay or
-                inability to use this web site / services or related services,
-                the provision of or failure to provide services, or for any
-                information, products, services and material obtained through
-                this web site, or otherwise arising out of the use of this web
-                site / services, whether based on contract, tort, negligence,
-                strict liability or otherwise, even if VJCS or any of its
-                subsidiaries has been advised of the possibility of damages. If
-                you are dissatisfied with any portion of this web site /
-                services, or with any of these terms of use, your sole and
-                exclusive remedy is to discontinue using this web site /
-                services.
-              </p>
+            <h5>International Users and Choice of Law</h5>
+            <p>
+              The foregoing are subject to the laws of the Republic of India and the
+              courts in Pune, India shall have the exclusive jurisdiction on any
+              dispute that may arise out of the use of this site.
+            </p>
 
-              <h5>Disclaimer</h5>
-              <p>
-                The web site may contain inaccuracies and typographical and
-                clerical errors. VJCS expressly disclaims any obligation to
-                update this site or any of the materials on this site. VJCS
-                reserves the right, in its sole discretion, to correct any
-                errors or omissions in any portion of the site. VJCS may make
-                any other changes to the site, the materials and the products,
-                programs, services described in the site at any time without
-                notice. VJCS does not warrant the accuracy or completeness of
-                the materials or the reliability of any advice, opinion,
-                statement or other information displayed or distributed through
-                the site. You acknowledge that any reliance on any such opinion,
-                advice, statement, memorandum, or information shall be at your
-                sole risk.
-              </p>
+            <p className="highlight">
+              Please proceed only if you accept all the conditions enumerated herein
+              above, out of your free will and consent.
+            </p>
 
-              <h5>Changes</h5>
-              <p>
-                VJCS reserves the rights, at its sole discretion, to change,
-                modify, add or remove any portion of these “Terms of Use” in
-                whole or in part, at any time. Changes in these “Terms of Use”
-                will be effective when notice of such change is posted. Your
-                continued use of the Web Site after any changes to these “Terms
-                of Use” are posted will be considered acceptance of those
-                changes. VJCS may terminate, change, suspend or discontinue any
-                aspect of the VJCS Web Site, including the availability of any
-                features of the Site, at any time. VJCS may also impose limits
-                on certain features and services or restrict your access to
-                parts or all of the Web Site without notice or liability. VJCS
-                may terminate the authorization, rights and license given above
-                and, upon such termination; you shall immediately destroy all
-                Materials.
-              </p>
+            <h5>Limitation of Damages</h5>
+            <p>
+              In no event shall VJCS and/or its subsidiaries be liable for any direct,
+              indirect, punitive, incidental, special, consequential damages or any
+              damages whatsoever including, without limitation, damages for loss of
+              use, data or profits, arising out of or in any way connected with the
+              use or performance of this web site/services.
+            </p>
 
-              
-            </div>
+            <h5>Disclaimer</h5>
+            <p>
+              The web site may contain inaccuracies and typographical errors. VJCS
+              expressly disclaims any obligation to update this site. VJCS reserves
+              the right to correct errors or omissions, and to make changes to the
+              site, the materials and the products, programs, or services described
+              without notice.
+            </p>
+
+            <h5>Changes</h5>
+            <p>
+              VJCS reserves the rights, at its sole discretion, to change, modify, add
+              or remove any portion of these “Terms of Use” at any time. Continued use
+              of the site after such changes means you accept those changes.
+            </p>
           </div>
         </div>
       </div>
