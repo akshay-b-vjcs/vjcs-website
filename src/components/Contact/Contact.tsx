e623757ba8bd99e101652e32cfedd94d1bfb01c8
Sayali <Sayali.bhujbal@vjcs.com>
2025-09-22 12:21:12 +0530

    Did changes in font and size of icons and also change the address

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -352,7 +352,7 @@ const Contact: React.FC = () => {
                 icon="bi-geo-alt-fill"
                 title={
                   <>
-                    Corporate Office, Pune<span>VJ Coresoft Pvt. Ltd.</span>
+                    Corporate Office, Pune <span>VJ Coresoft Pvt. Ltd.</span>
                   </>
                 }
                 description={
@@ -381,11 +381,7 @@ const Contact: React.FC = () => {
             <div className="col-lg-4 col-md-6">
               <Card
                 icon="bi-geo-alt-fill"
-                title={
-                  <>
-                    Affiliate Office, USA<span>VJ Coresoft Pvt. Ltd.</span>
-                  </>
-                }
+                title={<>Affiliate Office, USA</>}
                 description={
                   <>
                     <span>
